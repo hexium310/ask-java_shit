7b73269fdd394c1c75dfcdcd92d12508c1254af8
hexium310 <hexium310@gmail.com>
2018-03-07 05:30:56 +0900

    First commit

---
+++ main.php
@@ -0,0 +1,21 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Dotenv\Dotenv;
+use mpyw\Cowitter\Client;
+
+$dotenv = new Dotenv(__DIR__);
+$dotenv->load();
+
+$client = new Client([
+    $_ENV['CONSUMER_KEY'],
+    $_ENV['CONSUMER_SECRET'],
+    $_ENV['ACCESS_TOKEN'],
+    $_ENV['ACCESS_TOKEN_SECRET'],
+]);
+
+$words = implode(' ', array_slice($argv, 1));
+$client->post ('statuses/update', [
+    'status' => "@java_shit 👉👉👉 {$words}🤔 👈👈👈 この英語教えて〜〜〜 🙏🐷🦂💖😀"
+]);
