3de4c27a702175796a95e43b5bf74c24b938be4e
Chitoku <odango@chitoku.jp>
2018-03-09 01:45:11 +0900

    Resolve #8: Add internationalization support

--- main.php
+++ main.php
@@ -3,7 +3,14 @@
 require __DIR__ . '/vendor/autoload.php';
 
 use Dotenv\Dotenv;
+use Gettext\Translations;
+use Gettext\Translator;
 use mpyw\Cowitter\Client;
+
+$locale = getenv('LANGUAGE') ?: getenv('LC_ALL') ?: getenv('LC_MESSAGES') ?: getenv('LANG');
+$locale = file_exists(__DIR__ . "/locale/{$locale}/LC_MESSAGES/messages.po") ? $locale : 'en_US';
+$translator = new Translator();
+$translator->loadTranslations(Translations::fromPoFile(__DIR__ . "/locale/{$locale}/LC_MESSAGES/messages.po"))->register();
 
 $dotenv = new Dotenv(__DIR__);
 $dotenv->load();
@@ -19,7 +26,7 @@ $words = implode(' ', array_slice($argv, 1));
 try {
     $target = getenv('JAVASHIT_ACCOUNT');
     $client->post('statuses/update', [
-        'status' => "@{$target} 👉👉👉 {$words}🤔 👈👈👈 この英語教えて〜〜〜 🙏🐷🦂💖😀",
+        'status' => __('Teach me this English', $target, $words),
     ]);
 } catch (\RuntimeException $e) {
     error_log($e->getMessage());
