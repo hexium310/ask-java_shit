3cfd164123c8f5e77fb070531244d1eed1b0ed89
Yuu Kobayashi <kb10uy@kb10uy.org>
2018-03-09 00:09:23 +0900

    Habe aktualisiert

--- maintest.php
+++ maintest.php
@@ -1,22 +1,29 @@
 <?php
+require __DIR__ . '/vendor/autoload.php';
+use Dotenv\Dotenv;
+use mpyw\Cowitter\Client;
 
 class MainTest
 {
-    public function testMain()
+    public function test()
     {
         $dotenv = new Dotenv(__DIR__);
         $dotenv->load();
         $client = new Client([
-            $_ENV['CONSUMER_KEY'],
-            $_ENV['CONSUMER_SECRET'],
-            $_ENV['ACCESS_TOKEN'],
-            $_ENV['ACCESS_TOKEN_SECRET'],
+            getenv('CONSUMER_KEY'),
+            getenv('CONSUMER_SECRET'),
+            getenv('ACCESS_TOKEN'),
+            getenv('ACCESS_TOKEN_SECRET'),
         ]);
         $words = implode(' ', array_slice($argv, 1));
-        $client->post('statuses/update', [
-            'status' => "@java_shit 👉👉👉 {$words}🤔 👈👈👈 この英語教えて〜〜〜 🙏🐷🦂💖😀",
-        ]);
+        try {
+            $client->post('statuses/update', [
+                'status' => "@java_shit 👉👉👉 {$words}🤔 👈👈👈 この英語教えて〜〜〜 🙏🐷🦂💖😀",
+            ]);
+        } catch (\RuntimeException $e) {
+            error_log($e->getMessage());
+        }
     }
 }
 
-(new MainTest)->testMain();
+(new MainTest)->test();
